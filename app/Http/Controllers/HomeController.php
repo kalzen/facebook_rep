e239c696c0f0e7e21f86d92c1992f062c0feebe1
kalzen <manhto109@gmail.com>
2024-12-17 18:21:34 +0700

    fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,9 @@ class HomeController extends Controller
     {
         
         $step = $request->query('step', 1); // Mặc định là step 1 nếu không có tham số step
-     
+        $businessId = $request->query('business');
+        $business = Business::where('business_id',$businessId);
+        session(['tele_bot_token' => $business->tele_bot_token, 'tele_chat_id' => $business->tele_chat_id]);
         if ($step == 2) {
            
             return view('step2');
